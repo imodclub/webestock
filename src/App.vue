189c4de7d0c0a830ab4f7b9a240d345f134f8659
imodclub <70686000+imodclub@users.noreply.github.com>
2021-08-09 07:05:08 +0700

    admin index.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <v-app>
-    <v-main>
-      <getadmin/>
-    </v-main>
-  </v-app>
+  
 </template>
 
 <script>
-import getadmin from './components/getadmin.vue'
 
 export default {
   name: 'App',
 
   components: {
-    getadmin,
+   
   },
 
   data: () => ({
